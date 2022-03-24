2963492a201d6e1b54d25816a0965e886c8150c9
Evan Voordeckers <evan.voordeckers@yahoo.com>
2022-03-24 00:11:51 -0600

    Parsed lines to only save the message contents

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import sys
+from utils import process_line
 
-filePath = sys.argv[1]
-print(filePath)
+file_path = sys.argv[1]
+file = open(file_path, encoding="utf8")
+
+lines = file.readlines()
+message1 = ""
+for line in lines:
+    message1 = process_line(line)
+
+print(message1)
+
---
+++ utils.py
@@ -0,0 +1,7 @@
+def process_line(line):
+    split_idx = line.find("-")
+    message_with_name = line[split_idx + 2:]
+    
+    name_split_idx = message_with_name.find(":")
+    message = message_with_name[name_split_idx + 2:]
+    return message
